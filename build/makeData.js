f9bd8ac7593a70d02a8ed54a402f12da7d88fb87
lmw6412036 <lmw6412036@sina.com>
2017-05-15 22:55:25 +0800

    增加babel-runtime以支持node6++

--- build/makeData.js
+++ build/makeData.js
@@ -2,41 +2,46 @@
  * Created by Administrator on 2017/5/12 0012.
  */
 //读取配置，生成配置文件
-var fs=require('fs')
-var glob=require('glob')
-var thunkify=require('thunkify')
-var tampArr=glob.sync(__dirname+'/../data/?(tags|service)/*.json')
+var fs = require('fs')
+var glob = require('glob')
+var thunkify = require('thunkify')
+var path = require('path')
+var tampArr = glob.sync(__dirname + '/../data/?(tags|service)/*.json')
 var tags = [], services = [], config = {}
 function run(fn) {
-  var gen = fn();
-  function next(err, data) {
-    var result = gen.next(data);
-    if (result.done) return;
-    result.value(next);
-  }
-  next();
+    var gen = fn();
+
+    function next(err, data) {
+        var result = gen.next(data);
+        if (result.done) return;
+        result.value(next);
+    }
+
+    next();
 }
 
-module.exports=function () {
-  run(function *() {
-    let file = __dirname + '/../data/config.json'
-    let res = yield thunkify(fs.readFile)(file, 'utf8');
-    config = JSON.parse(res);
-    for(i in tampArr){
-      res = yield thunkify(fs.readFile)(tampArr[i], 'utf8');
-      if(tampArr[i].indexOf('tags')>=0){
-        tags.push(JSON.parse(res));
-      }else if(tampArr[i].indexOf('service')>=0){
-        services.push(JSON.parse(res));
-      }
-      config.tags = tags;
-      config.services = services;
-    }
-    //
-    fs.writeFile(file,JSON.stringify(config,function (key,value) {
-      return value;
-    },2),function (err,res) {
-      console.log('ok');
-    })
-  });
+module.exports = function () {
+    run(function *() {
+        let file = __dirname + '/../data/config.json'
+        let res = yield thunkify(fs.readFile)(file, 'utf8');
+        config = JSON.parse(res);
+        for (i in tampArr) {
+            res = yield thunkify(fs.readFile)(tampArr[i], 'utf8')
+            res = JSON.parse(res)
+            res.id=(path.basename(tampArr[i], '.json'))
+            if (tampArr[i].indexOf('tags') >= 0) {
+                tags.push(res);
+            } else if (tampArr[i].indexOf('service') >= 0) {
+                services.push(res);
+            }
+            config.tags = tags;
+            config.services = services;
+        }
+        //
+        fs.writeFile(file, JSON.stringify(config, function (key, value) {
+            return value;
+        }, 2), function (err, res) {
+            console.log('ok');
+        })
+    });
 }
--- src/app/home/Index.vue
+++ src/app/home/Index.vue
@@ -1,111 +1,112 @@
 <template>
     <div>
-      <h3>{{data.name}}</h3>
-      <div class="description">{{data.description}}</div>
-      <h4>基本配置</h4>
-      <dl class="baseconfig">
-        <dt>URL:</dt><dd>{{data.URL}}</dd>
-      </dl>
-      <dl class="baseconfig">
-        <dt>基础参数:</dt><dd><pre>{{baseconfig}}</pre></dd>
-      </dl>
-      <div class="tags">
-        <ul>
-          <li v-for="(item,index) in tags">
-            <h5 @click="showChildren(item)">
-              <b>{{index+1}}、{{item.name}}</b>
-              <span>{{item.description}}</span>
-            </h5>
-            <ol v-if="item.show">
-              <li>
-                1
-              </li>
-              <li>
-                2
-              </li>
-              <li>
-                3
-              </li>
-            </ol>
-          </li>
-        </ul>
-      </div>
+        <h3>{{data.name}}</h3>
+        <div class="description">{{data.description}}</div>
+        <h4>基本配置</h4>
+        <dl class="baseconfig">
+            <dt>URL:</dt>
+            <dd>{{data.URL}}</dd>
+        </dl>
+        <dl class="baseconfig">
+            <dt>基础参数:</dt>
+            <dd>
+                <pre>{{baseconfig}}</pre>
+            </dd>
+        </dl>
+        <div class="tags">
+            <ul>
+                <li v-for="(item,index) in tags">
+                    <h5 @click="showChildren(item)">
+                        <b>{{index + 1}}、{{item.name}}</b>
+                        <span>{{item.description}}</span>
+                    </h5>
+                    <ol v-if="item.show">
+                        <li></li>
+                    </ol>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
-import json from '../../../data/config.json'
-export default {
-    name: 'homeIndex',
-    data () {
-        return {
-            data: json
-        }
-    },
-    computed: {
-        baseconfig () {
-            return JSON.stringify(this.data.params, function (k, v) {
-              return v
-            }, 2)
+    import json from '../../../data/config.json'
+    export default {
+        name: 'homeIndex',
+        data () {
+            return {
+                data: json
+            }
         },
-        tags () {
-          this.data.tags = this.data.tags.map((item, index) => {
-            item = Object.assign({}, item, {show: false})
-            return item
-          })
-          return this.data.tags
+        computed: {
+            baseconfig () {
+                return JSON.stringify(this.data.params, function (k, v) {
+                    return v
+                }, 2)
+            },
+            tags () {
+                this.data.tags = this.data.tags.map((item, index) => {
+                    item = Object.assign({}, item, {show: false})
+                    return item
+                })
+                return this.data.tags
+            }
+        },
+        methods: {
+            showChildren (item) {
+                this.$set(item, "show", !item.show)
+            }
         }
-    },
-    methods: {
-      showChildren (item) {
-        this.$set(item, "show", !item.show)
-      }
     }
-}
 </script>
 <style scoped lang="scss">
-  h3{
-    margin: 20px 0;
-  }
-  .description{
-    color: #999;
-    font-size: 14px;
-  }
-  h4{
-    margin: 10px 0;
-  }
-  .baseconfig{
-    padding-bottom: 10px;
-    overflow: hidden;
-    dt,dd{
-      float: left;
+    h3 {
+        margin: 20px 0;
     }
-    dt{
-      width: 100px;
+
+    .description {
+        color: #999;
+        font-size: 14px;
     }
-  }
-  pre{
-    padding: 5px;
-    background-color: #f0f0f0;
-    line-height: 2;
-  }
-  .tags{
-    margin-top:10px;
-    ul>li+li{
-      border-top: 1px solid #f0f0f0;
+
+    h4 {
+        margin: 10px 0;
     }
-    ul>li{
-      h5{
-        line-height: 40px;
+
+    .baseconfig {
+        padding-bottom: 10px;
         overflow: hidden;
-        cursor: pointer;
-        b{
-          float: left;
+        dt, dd {
+            float: left;
         }
-        span{
-          float: right;
-          color: #999;
+        dt {
+            width: 100px;
         }
-      }
     }
-  }
+
+    pre {
+        padding: 5px;
+        background-color: #f0f0f0;
+        line-height: 2;
+    }
+
+    .tags {
+        margin-top: 10px;
+        ul > li + li {
+            border-top: 1px solid #f0f0f0;
+        }
+        ul > li {
+            h5 {
+                line-height: 40px;
+                overflow: hidden;
+                cursor: pointer;
+                b {
+                    float: left;
+                }
+                span {
+                    float: right;
+                    color: #999;
+                }
+            }
+        }
+    }
 </style>
